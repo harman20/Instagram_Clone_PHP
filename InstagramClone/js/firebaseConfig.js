34b21680e7ecaab02f9afe52a670951a767d6b8a
Harmanjit Singh <57390891+harman20@users.noreply.github.com>
2021-06-15 21:45:35 +0530

    Add files via upload

---
+++ InstagramClone/js/firebaseConfig.js
@@ -0,0 +1,14 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyA-5bVsSjAnro4T-0CZu2BtiuuorskofYM",
+    authDomain: "instagram-clone-c9a38.firebaseapp.com",
+    projectId: "instagram-clone-c9a38",
+    storageBucket: "instagram-clone-c9a38.appspot.com",
+    messagingSenderId: "415294626914",
+    appId: "1:415294626914:web:2d82eb46e997dfac91453b"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+const auth = firebase.auth();
+
+
